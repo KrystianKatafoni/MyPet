c0e412a5d9a5a272ec531c57a55bad95c49477f9
Krystian Katafoni <krystian.kat@gmail.com>
2017-10-29 23:43:04 +0100

    Blank application with dependencies.

---
+++ src/main/java/net/azurewebsites/mypet/MypetApplication.java
@@ -0,0 +1,12 @@
+package net.azurewebsites.mypet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypetApplication.class, args);
+	}
+}
