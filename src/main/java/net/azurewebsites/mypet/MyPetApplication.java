a174a4f0ec843b914911213c485a9dfb7aa25173
Krystian Katafoni <krystian.kat@gmail.com>
2017-10-30 20:51:59 +0100

    Added configurations: mysql and circleci.

--- src/main/java/net/azurewebsites/mypet/MyPetApplication.java
+++ src/main/java/net/azurewebsites/mypet/MyPetApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MypetApplication {
+
+public class MyPetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MypetApplication.class, args);
+		SpringApplication.run(MyPetApplication.class, args);
 	}
 }
