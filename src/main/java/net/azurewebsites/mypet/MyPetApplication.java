563140f9b9922cf41542f66ed7d26c747e5f7c30
Krystian Katafoni <krystian.kat@gmail.com>
2017-10-30 20:58:03 +0100

    Excluded DataSourceAutoConfiguration.

--- src/main/java/net/azurewebsites/mypet/MyPetApplication.java
+++ src/main/java/net/azurewebsites/mypet/MyPetApplication.java
@@ -1,10 +1,12 @@
 package net.azurewebsites.mypet;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class MyPetApplication {
 
 	public static void main(String[] args) {
